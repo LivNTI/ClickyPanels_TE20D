2ab8b1eaa6ffcc71aedc7a50b9d143870fbfe436
LivNTI <107177980+LivNTI@users.noreply.github.com>
2023-02-21 14:29:09 +0100

    First

--- src/Bakgrund.java
+++ src/Bakgrund.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Bakgrund {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Bakgrund extends JPanel implements MouseListener {
+    //Attrib
+    final int SIZE =400;
+    private boolean firstcolor= false;
+    //constr
+    public Bakgrund(){
+        this.setBackground(Color.BLACK);
+        this.setPreferredSize(new Dimension(SIZE,SIZE));
+        this.addMouseListener(this);
+
+    }
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("YOU HAVE CLICKED!");
+        if (!firstcolor) {
+            this.setBackground(Color.CYAN);
+            //firstcolor=false;
+        } else{
+            this.setBackground(Color.YELLOW);
+            //firstcolor=true;
+        }
+
+        firstcolor= !firstcolor;
+
+
+        repaint();
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    //Methods
+
+    //getset
+
 }
--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Window {
+import javax.swing.*;
+
+public class Window extends JFrame {
+    //Attrib
+    //constr
+    public Window(){
+        //Window design and function
+        this.setTitle("A Clicker");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Window content
+this.add(new Bakgrund());
+
+        //window visible
+        this.pack();
+        this.setVisible(true);
+
+    }
+
+    //method
+
+    //GetSet
+
 }
